627cd31d4af632e0cfe9abc676cb73f65a4e0b40
basselalsayed <alsayed.bassel@gmail.com>
2023-01-02 14:24:55 +0300

    quick win moving getters to Item class

--- src/gilded_rose_refactored.js
+++ src/gilded_rose_refactored.js
@@ -1,90 +1,88 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  get isSulfuras() {
+    return this.name === 'Sulfuras, Hand of Ragnaros';
+  }
+  get isBackstagePass() {
+    return this.name === 'Backstage passes to a TAFKAL80ETC concert';
+  }
+  get isAgedBrie() {
+    return this.name === 'Aged Brie';
+  }
+  get isSpecial() {
+    return this.isBackstagePass || this.isAgedBrie;
+  }
 }
 
 class Shop {
-  constructor(items=[]) {
+  constructor(items = []) {
     this.items = items;
   }
-  isSulfuras(item) {
-    return (item.name === 'Sulfuras, Hand of Ragnaros')
-  }
-  isBackstagePass(item) {
-    return (item.name === 'Backstage passes to a TAFKAL80ETC concert') 
-  }
-  isAgedBrie(item) {
-    return (item.name === 'Aged Brie')
-  }
-  isSpecial(item) {
-    return (this.isBackstagePass(item) || this.isAgedBrie(item) )
-  }
+
   updateSpecialProperty(item, q) {
-    if ((item.quality + q) < 50) {
-      return item.quality += q
+    if (item.quality + q < 50) {
+      item.quality += q;
     } else {
-      return item.quality = 50
+      item.quality = 50;
     }
   }
   updateNormal(item) {
-    if (item.quality > 0) {
-      return item.quality -= 1
-    }
+    if (item.quality > 0) item.quality -= 1;
   }
   updateBackstagePass(item) {
     if (item.sellIn <= 10) {
       this.updateSpecialProperty(item, 2);
       if (item.sellIn < 0) {
-          return item.quality = 0
+        item.quality = 0;
       } else if (item.sellIn <= 5) {
-          this.updateSpecialProperty(item, 1);
+        this.updateSpecialProperty(item, 1);
       }
     }
   }
   updateAgedBrie(item) {
-    this.updateSpecialProperty(item, 1)
+    this.updateSpecialProperty(item, 1);
     if (item.sellIn <= 0) {
-      this.updateSpecialProperty(item, 1)
+      this.updateSpecialProperty(item, 1);
     }
   }
   updateSpecial(item) {
-    if (this.isBackstagePass(item)) {
-        this.updateBackstagePass(item)
-    } else if (this.isAgedBrie(item)) {
-        this.updateAgedBrie(item)
-    }
+    if (item.isBackstagePass) this.updateBackstagePass(item);
+
+    if (item.isAgedBrie) this.updateAgedBrie(item);
   }
+
   updateAll(item) {
-    if (this.isSulfuras(item)) {
-      return item
-    } 
-    if (this.isSpecial(item)) {
-        this.updateSpecial(item)
+    if (item.isSulfuras) {
+      return item;
+    }
+    if (item.isSpecial) {
+      this.updateSpecial(item);
     } else {
-        this.updateNormal(item)
+      this.updateNormal(item);
     }
     this.addDay(item);
-    return item
+    return item;
   }
+
   addDay(item) {
     if (item.quality < 50) {
-      return item.sellIn --
+      item.sellIn--;
     }
   }
-  updateQuality() {
-    this.items.forEach((item) => {
-      this.updateAll(item)
-    })
 
-    return this.items
-    
+  updateQuality() {
+    this.items = this.items.map(this.updateAll.bind(this));
+
+    return this.items;
   }
 }
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
